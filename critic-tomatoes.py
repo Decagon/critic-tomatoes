ed6d4a74914b74fcd129de7f9d44dfd8541a3c8b
Decagon <Decagon@users.noreply.github.com>
2017-12-31 20:10:46 -0400

    Combine reader.py; add interactive prompt

--- critic-tomatoes.py
+++ critic-tomatoes.py
@@ -2,11 +2,11 @@ from bs4 import BeautifulSoup
 import sys
 from urllib.request import urlopen
 import pickle
-sys.setrecursionlimit(20000)
-criticUrls = ["https://www.rottentomatoes.com/critic/rex-reed/movies", "https://www.rottentomatoes.com/critic/anthony-lane/movies"]
-for criticUrl in criticUrls:
-    criticUrl = criticUrl + "?page="
-    critic = criticUrl.split("/")[4]
+
+critics = ["rex-reed", "anthony-lane"]
+
+for critic in critics:
+    criticUrl = "https://www.rottentomatoes.com/critic/" + critic + "/movies?page="
 
     # find the database file, if it exists
     dbExists = False
@@ -21,44 +21,104 @@ for criticUrl in criticUrls:
     soup = BeautifulSoup(r, "lxml")
 
     # find how many pages this reviewer has
-    stopPage = soup.find("section", {"class": "panel panel-rt panel-box scrollable-table"}).find("ul", {"class": "pagination"}).findAll("a")[4].get("href")
+    stopPage = soup.find("section", {"class": "panel panel-rt panel-box scrollable-table"}).find(
+        "ul", {"class": "pagination"}).findAll("a")[4].get("href")
     stopPageInt = int(stopPage.split("=")[1])
     counter = 0
 
     # assuming that only one page of reviews has been added since the last time
     if (dbExists):
         # wasteful since this should be checked earlier
-        stopPageInt = 1;
+        stopPageInt = 1
     # bit of a waste since the first page is captured twice
     # keep scraping until we reach the end (since RT doesn't have 404's when we hit the end)
     while (counter < stopPageInt):
         counter = counter + 1
-        print("Loading " + critic + "'s review page " + str(counter) + " of " + str(stopPageInt) + "...")
+        print("Loading " + critic + "'s review page " +
+              str(counter) + " of " + str(stopPageInt) + "...")
         # might contain duplicate ratings (how to fix?)
         r = urlopen(criticUrl + str(counter)).read()
-        soup2= BeautifulSoup(r, "lxml")
-        mydivs = soup2.findAll("table", { "class" : "table table-striped" })
+        soup2 = BeautifulSoup(r, "lxml")
+        mydivs = soup2.findAll("table", {"class": "table table-striped"})
         for div in mydivs:
-                for tr in div.findAll("tr"):
-                        rating = tr.find("span")
-                        if (rating is None):
-                            continue
+            for tr in div.findAll("tr"):
+                rating = tr.find("span")
+                if (rating is None):
+                    continue
 
-                        # sometimes there will be no rating, just a tomato or splat
-                        # so give the splats a zero score, and a tomato a one
-                        if (rating["title"] == ""):
-                            if (rating["class"] == "icon tiny fresh"):
-                                rating = "1/1"
-                            else:
-                                rating = "0/1"
-                        else:
-                            rating = rating["title"]
+                # sometimes there will be no rating, just a tomato or splat
+                # so give the splats a zero score, and a tomato a one
+                if (rating["title"] == ""):
+                    if (rating["class"] == "icon tiny fresh"):
+                        rating = "1/1"
+                    else:
+                        rating = "0/1"
+                else:
+                    rating = rating["title"]
 
-                        try:
-                            movie = tr.find("a").contents[0]
-                            if (movie is not None):
-                                criticRatings[str(tr.find("a").contents[0])] = str(rating)
-                        except IndexError:
-                            pass
+                try:
+                    movie = tr.find("a").contents[0]
+                    if (movie is not None):
+                        criticRatings[str(tr.find("a").contents[0])] = str(
+                            rating)
+                except IndexError:
+                    pass
     print("Saving database...")
     pickle.dump(criticRatings, open(critic + ".pickle", "wb"))
+
+
+# show user interactive prompt to search for movies and their ratings
+ratings = []
+
+for critic in critics:
+    ratings.append(pickle.load(open(critic + ".pickle", "rb")))
+
+
+def parseDivide(inputVal):
+    inputVal = inputVal.split("/")
+    numerator = inputVal[0]
+    denominator = inputVal[1]
+    return str(float(numerator) / float(denominator))
+
+
+moviesAllReviewed = ratings[0]
+
+counter = 1
+while (counter < len(ratings)):
+    moviesAllReviewed = moviesAllReviewed & ratings[counter].keys()
+    counter = counter + 1
+
+# lowercase all movies to help with searching
+moviesAllReviewed = [x.lower() for x in moviesAllReviewed]
+
+ratingsTmp = []
+for rating in ratings:
+    ratingsTmp.append({k.lower(): v for k, v in rating.items()})
+
+ratings = ratingsTmp
+
+def calculateCombinedRating(movieName):
+    counter = 0
+    movieName = movieName.lower()
+    localRatings = []
+    while (counter < len(ratings)):
+        criticRating = parseDivide(ratings[counter][movieName])
+        print(critics[counter] + " rated " +
+              movieName + ": " + str(criticRating))
+        counter = counter + 1
+        localRatings.append(float(criticRating))
+
+    combinedRating = sum(localRatings) / len(localRatings)
+    print("Combined rating: " + str(round(combinedRating, 2)))
+
+
+while True:
+    try:
+        response = input(
+            "Enter a movie name (press Ctrl-C to quit; type 'show all movies' to show all movies reviewed by all critics):")
+        if (response == "show all movies"):
+            print(moviesAllReviewed)
+        else:
+            calculateCombinedRating(response)
+    except KeyError:
+        print("Movie was not reviewed by all critics.")
